13cd3b3ef6e701d94eeca232e59fc6594b82d866
Adrian Epifanio Rodriguez Hernandez <adrianepi@gmail.com>
2025-03-28 20:18:39 +0000

    Updated doc

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ author = 'Adrian Epifanio'
 extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['docs/_build', 'Thumbs.db', '.DS_Store']
 
 
 
@@ -28,4 +28,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = ['docs/_static']
